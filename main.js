32dd75e60c936a369dee5f808ccb06543fcfe94e
bud aminof <budaminof@gmail.com>
2016-03-01 09:49:54 -0700

    google map is putting markers and clearing them on a new search

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // $(document).ready( function () {
-  console.log('we are on');
+  // console.log('we are on');
 
 //setting today's date.
   var today = new Date();
@@ -14,80 +14,83 @@
   todayH1 = dd+'.'+mm+'.'+yyyy;
   $('h1').append('<small>'+todayH1+'</small>');
 
-  $('#location').on('click', function (){
-    $('#table').empty();
-    // setMapOnAll(null);
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      // center: {lat: 40.018228, lng: -105.2864265},
+      zoom: 14,
+      mapTypeId: google.maps.MapTypeId.ROADS //SATELLITE ROADS TERRAIN HYBRID
+    })
+  }
 
-    var city = ($('input[name="city"]')).val();
-    var state = ($('input[name="state"]')).val();
-    var miles = ($('input[name="radius"]')).val();
+  var markers = [];
 
-//getting api information
+  function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(map);
+    }
+  }
 
+  function clearMarkers() {
+    setMapOnAll(null);
+  }
+
+//searching for events.
+
+$('#location').on('click', function (){
+
+  clearMarkers();
+  $('body').css('background', 'none');
+  $('aside').css({'align-items': 'flex-start','justify-content': 'flex-start', 'padding-left': '1.5%'});
+  $('input, button').css({'font-size': '100%', 'padding': '0', 'width': '10%'});
+  $('#map').show();
+  google.maps.event.trigger(map, 'resize');
+  $('#table').empty();
+
+  var city = ($('input[name="city"]')).val();
+  var state = ($('input[name="state"]')).val();
+  var miles = ($('input[name="radius"]')).val();
+
+
+    function addMarker(location,label) {
+      var marker =new google.maps.Marker({
+        position: location,
+        map: map,
+        label: label
+      });
+      var infoWindow = new google.maps.InfoWindow()
+
+      marker.addListener('click', function() {
+        infoWindow.setContent(this.label);
+        infoWindow.open(map, this)
+      })
+      markers.push(marker);
+    }
+
+    console.log("foo");
     $.ajax({
-      url: 'https://api.bandsintown.com/events/search.json?api_version=2.0&app_id=CoolApp&location='+city+','+state+'&radius='+miles+'&date='+today,
+      url: 'https://api.bandsintown.com/events/search.json?api_version=2.0&app_id=CoolApp&location='+ city +','+ state +'&radius='+ miles +'&date='+ today,
       type: 'GET',
       dataType: 'jsonp',
       success: function(res) {
-        markers = [];
 
         for (var i = 0; i < res.length; i++) {
-          arr = []
+          // arr = []
           var musician = res[i].artists[0].name;
           var venue = res[i].venue.name;
           var url = res[i].venue.url;
           var venueLat = res[i].venue.latitude;
           var venueLng = res[i].venue.longitude;
+          map.setCenter({lat: venueLat, lng: venueLng})
           $('#table').append('<tr><td>'+ musician +'</td><td>'+ venue + '</td><td><a href='+ url +' target="_blank">Tickets</a></td></tr>');
-          arr.push(venue, venueLat, venueLng);
-          markers.push(arr);
+          // arr.push(venue, venueLat, venueLng);
+          // markers.push(arr);
+          addMarker({lat: venueLat, lng: venueLng}, venue);
         }
-
-        // function setMapOnAll(map) {
-        //
-        //   for (var i = 0; i < markers.length; i++) {
-        //
-        //     var position = {lat: markers[i][1], lng: markers[i][2]};
-        //
-        //     marker = new google.maps.Marker({
-        //       position: position,
-        //       map: map,
-        //       title: markers[i][0]
-        //     })
-        //   }
-        // }
 
       }
     });
-  });
+
+});
 
 // }); // document ready end.
-
-///google map
-
-var map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.018228, lng: -105.2864265},
-    zoom: 14,
-    mapTypeId: google.maps.MapTypeId.ROADS //SATELLITE ROADS TERRAIN HYBRID
-  })
-}
-
-
-
-// // 2. Adding an infoWindow to the marker
-// var infoWindow = new google.maps.InfoWindow()
-//
-// marker.addListener('click', function() {
-//   infoWindow.setContent(this.title);
-//   infoWindow.open(map, this)
-// })
-
-  // var content = "<h1>I'm Content!</h1>"
-  //
-  // marker.addListener('click', function() {
-  //   infoWindow.setContent(content);
-  //   infoWindow.open(map, this)
-  // })
