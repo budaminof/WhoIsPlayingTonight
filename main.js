a061a107397ae7d05cd27df4b6656fde14721ce3
bud aminof <budaminof@gmail.com>
2016-02-29 17:19:58 -0700

    starting a new expierament- wrapping success funciton

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // $(document).ready( function () {
   console.log('we are on');
 
+//setting today's date.
   var today = new Date();
   var dd = today.getDate();
   var mm = today.getMonth()+1;
@@ -14,7 +15,8 @@
   $('h1').append('<small>'+todayH1+'</small>');
 
   $('#location').on('click', function (){
-    $('#main').empty();
+    $('#table').empty();
+    // setMapOnAll(null);
 
     var city = ($('input[name="city"]')).val();
     var state = ($('input[name="state"]')).val();
@@ -27,30 +29,41 @@
       type: 'GET',
       dataType: 'jsonp',
       success: function(res) {
+        markers = [];
+
         for (var i = 0; i < res.length; i++) {
+          arr = []
           var musician = res[i].artists[0].name;
           var venue = res[i].venue.name;
           var url = res[i].venue.url;
-          $('#tabel').append('<tr><td>'+ musician +'</td><td>'+ venue + '</td><td><a href='+ url +' target="_blank">Tickets</a></td></tr>');
+          var venueLat = res[i].venue.latitude;
+          var venueLng = res[i].venue.longitude;
+          $('#table').append('<tr><td>'+ musician +'</td><td>'+ venue + '</td><td><a href='+ url +' target="_blank">Tickets</a></td></tr>');
+          arr.push(venue, venueLat, venueLng);
+          markers.push(arr);
         }
-      }
 
+        // function setMapOnAll(map) {
+        //
+        //   for (var i = 0; i < markers.length; i++) {
+        //
+        //     var position = {lat: markers[i][1], lng: markers[i][2]};
+        //
+        //     marker = new google.maps.Marker({
+        //       position: position,
+        //       map: map,
+        //       title: markers[i][0]
+        //     })
+        //   }
+        // }
+
+      }
     });
   });
 
-///google map
-//
-// function initMap() {
-//   var mapDiv = document.getElementById('map');
-//   var map = new google.maps.Map(mapDiv, {
-//     center: {lat: 44.540, lng: -78.546},
-//     zoom: 8
-//   });
-// }
-
 // }); // document ready end.
 
-//MAP TEST WITH MARKERS
+///google map
 
 var map;
 
@@ -60,50 +73,21 @@ function initMap() {
     zoom: 14,
     mapTypeId: google.maps.MapTypeId.ROADS //SATELLITE ROADS TERRAIN HYBRID
   })
+}
 
-  var markers = [
-    ['foo', 40.018228, -105.2864265 ],
-    ['bar', 40.01, -105.29 ],
-    ['baz', 40.02, -105.275 ],
-    ['buzz', 40.025, -105.2864265 ],
-  ];
 
-// 1. Adding a basic marker to the map
-
-  // var marker = new google.maps.Marker({
-  // position: {lat: 40.018228, lng: -105.2864265},
-  // map: map,
-  // title: 'Hello World!'
-  // });
 
 // // 2. Adding an infoWindow to the marker
+// var infoWindow = new google.maps.InfoWindow()
+//
+// marker.addListener('click', function() {
+//   infoWindow.setContent(this.title);
+//   infoWindow.open(map, this)
+// })
 
-  var infoWindow = new google.maps.InfoWindow()
-  //
   // var content = "<h1>I'm Content!</h1>"
   //
   // marker.addListener('click', function() {
   //   infoWindow.setContent(content);
   //   infoWindow.open(map, this)
   // })
-
-// 3. Adding multiple markers to the map
-
-
-  for (var i = 0; i < markers.length; i++) {
-
-    var position = {lat: markers[i][1], lng: markers[i][2]}
-
-    marker = new google.maps.Marker({
-        position: position,
-        map: map,
-        title: markers[i][0]
-    })
-
-    marker.addListener('click', function() {
-      infoWindow.setContent(this.title);
-      infoWindow.open(map, this)
-    })
-
-  }
-}
