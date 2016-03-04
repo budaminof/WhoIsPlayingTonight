7926ec163d7a2c994ecfe6ea49a8c59dde6e98f9
bud aminof <budaminof@gmail.com>
2016-03-04 12:27:41 -0700

    morning changes with Fabio

--- README.md
+++ README.md
@@ -12,29 +12,3 @@ this is officially a GOOGLE MAP API project.
 apparently there is a black hole here..
 
 ###
-
-
-while everyone are building rockets ships around me- I am building tiny google maps markers.
-I wanted to create an app that shows you every event happening today.
-But couldn't find an API that I know how to use with all this information in it.
-So I took this music events API.
-It was a very rough beginning- not being able to make what I wanted (someone said- attachment?1) was frustrating and I thought
-"ho... making an event list of today with a map is not a big deal."
-apparently there is a black hole somewhere in Google headquarters.
-Someone thought that a documentation in which when you scroll down the side bar doesn't move because it is so long- is funny.
-and so- my music events project became a google map project.
-starting with API to show the map.
-setting markers for each venue.
-finding my geolocation(! so freaking cool !).
-and than being overwhelmed by the direction documentation- "is this english?! because this is a verb and this is a noun and
-I think this is supposed to be a sentence but I can't understand it."
-with the support of Dize, Akyuna, Emily, Matt, Fabio and Luke I made it.
-This for me was a process of relearning everything that I learned in the last month.
-You think you understand scope?! not until you handled this delicate google.mapInit function.
-You think You don't understand what is an API? than use 3 of them in one project.
-You know what localStorage actually is? Ask Akyuna to explain it in the detailed way, and than add local storage to everything
-on your site, only to discover that it will break your delicate google.mapInit function. But at least you know how it works.
-
-To everyone who was there and supported me- thank you for dealing with me.
-
-FYI- my map shows only biking direction- welcome to the revolution.
--- main.js
+++ main.js
@@ -1,207 +1,186 @@
-console.log('we are on');
-//setting today's date
+(function(){
+    // expose initMap to global for googleMap's callback
+    initMap = initMapFn;
 
-  var today = new Date();
-  var dd = today.getDate();
-  var mm = today.getMonth()+1;
-  var yyyy = today.getFullYear();
+    //variable declaration.
+    var map, myLat, myLng, desLat, desLng, directionsDisplay, directionsService;
 
-  if (dd < 10) dd = '0'+dd;
-  if (mm < 10) mm = '0'+mm;
+    //setting today's date
+    var today = new Date().toISOString().slice(0, 10).split('-'),
+        year = today[0],
+        month = today[1],
+        day = today[2],
+        apiToday = today.join('-');
 
-  today = yyyy+'-'+mm+'-'+dd;
-  todayH1 = dd+'.'+mm+'.'+yyyy;
-  $('h1').append('<small>'+todayH1+'</small>');
+    $('h1').append('<small>' + [month, day, year].join('.') + '</small>');
 
-//map declaration.
-  var map;
-  var myLat;
-  var myLng;
-  var desLat;
-  var desLng;
-  var directionsDisplay;
-  var directionsService;
+    function initMapFn() {
+        // clear directions div
+        $('#directions').empty();
 
-  function initMap() {
-    directionsDisplay = new google.maps.DirectionsRenderer;
-    directionsService = new google.maps.DirectionsService;
+        // initialize and clear map
+        directionsDisplay = new google.maps.DirectionsRenderer;
+        directionsDisplay.setMap();
 
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 14,
-    mapTypeId: google.maps.MapTypeId.ROADS //SATELLITE ROADS TERRAIN HYBRID
-  })
-  directionsDisplay.setMap(map);
-  directionsDisplay.setPanel(document.getElementById('directions'));
+        directionsService = new google.maps.DirectionsService;
 
-    // check for Geolocation support
+        map = new google.maps.Map(document.getElementById('map'), {
+            mapTypeId: google.maps.MapTypeId.ROADS,
+            zoom: 14
+        })
+        directionsDisplay.setMap(map);
+        directionsDisplay.setPanel(document.getElementById('directions'));
 
-    if (navigator.geolocation) console.log('Geolocation is supported!');
-    else alert('Geolocation is not supported for this Browser/OS version yet.');
+        window.onload = function() {
+            var startPosition;
+            var geoSuccess = function(position) {
+                startPosition = position;
+                myLat = startPosition.coords.latitude;
+                myLng = startPosition.coords.longitude;
+                var startingMarker = new google.maps.Marker({
+                    position: {
+                        lat: myLat,
+                        lng: myLng
+                    },
+                    map: map,
+                    animation: google.maps.Animation.DROP
+                });
 
-    window.onload = function() {
-      var startPosition;
-      var geoSuccess = function(position) {
-        startPosition = position;
-        myLat = startPosition.coords.latitude;
-        myLng = startPosition.coords.longitude;
-        var startingMarker = new google.maps.Marker({
-          position: {lat: myLat, lng: myLng},
-          map: map,
-          icon: {
-              path: google.maps.SymbolPath.CIRCLE,
-              scale: 7
-            },
-          animation: google.maps.Animation.DROP
+                startingMarker.setMap(map);
+                var infoWindow = new google.maps.InfoWindow();
+
+                startingMarker.addListener('click', function() {
+                    infoWindow.setContent("you are here");
+                    infoWindow.open(map, this)
+                })
+
+            };
+            navigator.geolocation.getCurrentPosition(geoSuccess);
+        };
+
+    }
+
+
+    //myFrom menu
+    $('.fa-reorder').on('click', function() {
+        $('.myForm').slideToggle('slow');
+    })
+
+    //searching for events.
+    $('#locationSearchForm').on('submit', function(event) {
+        event.preventDefault();
+
+        // reinitialize map
+        initMapFn();
+        var city = $('input[name="city"]').val();
+        var state = $('input[name="state"]').val();
+        var miles = $('input[name="radius"]').val();
+        var filterCity = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/;
+
+        if (!filterCity.test(city)) {
+            $('input[name="city"]').addClass('danger');
+            return false;
+        }
+
+        if ($('input[name="state"]').val() == "") {
+            $('input[name="state"]').addClass('danger');
+            return false;
+        }
+
+        $('#mainAppend').hide();
+        $('body').css('background', 'none');
+        $('.myForm').css({'margin': '10px 0 0 0'});
+        $('input, button').addClass('happening');
+        $('.fa-reorder').show();
+        $('#map').show();
+        $('aside').show();
+        google.maps.event.trigger(map, 'resize');
+        $('#table').empty();
+        $('.myForm').hide();
+        $('i').addClass('burgerTime');
+        $('main').addClass('after');
+        $('#table').removeClass('error');
+        $('input').removeClass('danger');
+        
+        ///google map markers
+        function addMarker(location, label) {
+            var marker = new google.maps.Marker({
+                position: location,
+                map: map,
+                label: label,
+                animation: google.maps.Animation.DROP
+            });
+
+            var infoWindow = new google.maps.InfoWindow()
+
+            marker.addListener('click', function() {
+                desLat = this.position.lat();
+                desLng = this.position.lng();
+                var contentString = label + '<br/><a href="https://www.google.com/maps/dir/' + myLat + ',' + myLng + '/' + desLat + ',' + desLng + '" target="_blank">directions</a>';
+                infoWindow.setContent(contentString);
+                infoWindow.open(map, this);
+                calcRoute(desLat, desLng);
+                $('#directions').show();
+            })
+        }
+
+        $.ajax({
+            url: 'https://api.bandsintown.com/events/search.json?api_version=2.0&app_id=CoolApp&location=' + city + ',' + state + '&radius=' + miles + '&date=' + apiToday,
+            type: 'GET',
+            dataType: 'jsonp',
+            success: function(res) {
+                for (var i = 0; i < res.length; i++) {
+                    var musician = res[i].artists[0].name;
+                    var venue = res[i].venue.name;
+                    var url = res[i].venue.url;
+                    var venueLat = res[i].venue.latitude;
+                    var venueLng = res[i].venue.longitude;
+                    map.setCenter({
+                        lat: myLat,
+                        lng: myLng
+                    });
+                    $('#table').append('<tr><td><h3>' + musician + '</h3></td><td>' + venue + '</td><td class="tickets"><a href=' + url + ' target="_blank">Tickets</a></td></tr>');
+                    addMarker({
+                        lat: venueLat,
+                        lng: venueLng
+                    }, venue);
+
+                }
+
+                if ($('#table').children().length == 0) {
+                    $('#table').append('<h1>What did you do to my program?!</h1>');
+                    $('#table').addClass('error');
+                }
+            }
+
         });
 
-        startingMarker.setMap(map);
-        var infoWindow = new google.maps.InfoWindow();
+        $(document).on('click', 'tr', function() {
+            var yourShowArr = [];
+            yourShowArr.push($(this)[0].innerHTML);
+            localStorage.setItem('history', yourShowArr);
+        });
+    });
 
-        startingMarker.addListener('click', function() {
-          infoWindow.setContent("you are here");
-          infoWindow.open(map, this)
-        })
+    function calcRoute(desLat, desLng) {
+        console.log(desLat, desLng);
+        start = new google.maps.LatLng(myLat, myLng);
+        end = new google.maps.LatLng(desLat, desLng);
 
-      };
-      navigator.geolocation.getCurrentPosition(geoSuccess);
-    };
+        directionsService.route({
+            origin: start,
+            destination: end,
+            travelMode: google.maps.TravelMode.BICYCLING
+        }, function(response, status) {
+            console.log(status);
+            console.log(response);
+            if (status === google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+            }
+        });
 
-  }
-
-////markers on map
-
-  var markers = [];
-  function setMapOnAll(map) {
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(map);
-    }
-  }
-
-  function clearMarkers() {
-    setMapOnAll(null);
-  }
-
-//navigation menu
-
-$('.fa-reorder').on('click', function () {
-    $('navigation').slideToggle('slow');
-  })
-
-//searching for events.
-
-$('#location').on('click', function (){
-
-  var city = $('input[name="city"]').val();
-  var state = $('input[name="state"]').val();
-  var miles = $('input[name="radius"]').val();
-
-  var filterCity = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/;
-  if(!filterCity.test(city)){
-    $('input[name="city"]').css({'background-color': 'rgb(246, 54, 54)'});
-    return false;
-  }
-
-  if($('input[name="state"]').val() == "") {
-    $('input[name="state"]').css({'background-color': 'rgb(246, 54, 54)'});
-    return false;
-  }
-
-  clearMarkers();
-  $('#mainAppend').hide();
-  $('body').css('background', 'none');
-  $('nav').css({'align-items': 'flex-start','justify-content': 'flex-start', 'padding-left': '1.5%', 'margin-top': '1%'});
-  $('input, button').addClass('happening');
-  $('.fa-reorder').show();
-  $('#map').show();
-  $('aside').show();
-  google.maps.event.trigger(map, 'resize');
-  $('#table').empty();
-  $('navigation').hide();
-  $('i').addClass('burgerTime');
-  $('main').addClass('after');
-  $('#table').removeClass('error');
-
-
-
-///google map markers
-    function addMarker(location,label) {
-
-      var marker = new google.maps.Marker({
-        position: location,
-        map: map,
-        label: label,
-        animation: google.maps.Animation.DROP
-      });
-
-      var infoWindow = new google.maps.InfoWindow()
-
-      marker.addListener('click', function() {
-        desLat = this.position.lat();
-        desLng = this.position.lng();
-        var contentString = label + '<br/><a href="https://www.google.com/maps/dir/' + myLat + ',' + myLng + '/'+ desLat +','+ desLng +'" target="_blank">directions</a>';
-        infoWindow.setContent(contentString);
-        infoWindow.open(map, this);
-        calcRoute(desLat, desLng);
-        $('#directions').show();
-      })
-      markers.push(marker);
     }
 
-    $.ajax({
-      url: 'https://api.bandsintown.com/events/search.json?api_version=2.0&app_id=CoolApp&location='+ city +','+ state +'&radius='+ miles +'&date='+ today,
-      type: 'GET',
-      dataType: 'jsonp',
-      success: function(res) {
-        for (var i = 0; i < res.length; i++) {
-          var musician = res[i].artists[0].name;
-          var venue = res[i].venue.name;
-          var url = res[i].venue.url;
-          var venueLat = res[i].venue.latitude;
-          var venueLng = res[i].venue.longitude;
-          map.setCenter({lat: myLat, lng: myLng});
-          $('#table').append('<tr><td><h3>'+ musician +'</h3></td><td>'+ venue + '</td><td class="tickets"><a href='+ url +' target="_blank">Tickets</a></td></tr>');
-          addMarker({lat: venueLat, lng: venueLng}, venue);
-
-        }
-
-        if($('#table').children().length == 0) {
-          $('#table').append('<h1>What did you do to my program?!</h1>');
-          $('#table').addClass('error');
-        }
-      }
-
-  });
-
-///local storage
-
-  $(document).on('click','tr', function () {
-    var yourShowArr= [];
-    yourShowArr.push($(this)[0].innerHTML);
-    localStorage.setItem('history', yourShowArr);
-  });
-
-///error if search done bad.
-
-});
-
-function calcRoute(desLat, desLng) {
-
-  start = new google.maps.LatLng(myLat, myLng);
-  end = new google.maps.LatLng(desLat, desLng);
-
-  directionsService.route({
-    origin: start,
-    destination: end,
-    travelMode: google.maps.TravelMode.BICYCLING
-      }, function(response, status) {
-    if (status === google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(response);
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
-
-}
-
-var myHtmlFromLocalStorage = localStorage.getItem('history');
-$('#mainAppend').append(myHtmlFromLocalStorage);
+    var myHtmlFromLocalStorage = localStorage.getItem('history');
+    $('#mainAppend').append(myHtmlFromLocalStorage);
+})();
